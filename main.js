6a7df104d70211e93c13f206d2073f375a46beb6
umbertofaenza <umberto.faenza@gmail.com>
2023-09-28 16:48:14 +0200

    print data in cards

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ createApp({
 
   mounted() {
     axios.get("./server.php").then((response) => {
-      this.listaDischi = response;
+      this.listaDischi = response.data;
     });
   },
 }).mount("#app");
